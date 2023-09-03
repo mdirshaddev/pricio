d11440ee90c3a13744de1b19a290356f211195b0
mdirshaddev <mdirshaddev+github@gmail.com>
2023-09-03 16:03:26 +0530

    config: 🛠️ next app config for pwa and sentry integration

--- apps/public/next.config.js
+++ apps/public/next.config.js
@@ -1,5 +1,25 @@
-const { withSentryConfig } = require('@sentry/nextjs');
+const path = require('path');
+const withPWAConfig = require('next-pwa');
 const { version } = require('./package.json');
+const { withSentryConfig } = require('@sentry/nextjs');
+
+const generateAppDirEntry = entry => {
+  const packagePath = require.resolve('next-pwa');
+  const packageDirectory = path.dirname(packagePath);
+  const registerJs = path.join(packageDirectory, 'register.js');
+
+  return entry().then(entries => {
+    // Register SW on App directory, solution: https://github.com/shadowwalker/next-pwa/pull/427
+    if (entries['main-app'] && !entries['main-app'].includes(registerJs)) {
+      if (Array.isArray(entries['main-app'])) {
+        entries['main-app'].unshift(registerJs);
+      } else if (typeof entries['main-app'] === 'string') {
+        entries['main-app'] = [registerJs, entries['main-app']];
+      }
+    }
+    return entries;
+  });
+};
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -23,11 +43,28 @@ const nextConfig = {
         hostname: 'images.unsplash.com' // Unsplash Images
       }
     ]
+  },
+  webpack: config => {
+    const entry = generateAppDirEntry(config.entry);
+    config.entry = () => entry;
+    return config;
   }
 };
 
-const nextConfigWithSentry = withSentryConfig(
-  nextConfig,
+/** @type {import('next-pwa').PWAConfig} */
+const withPWA = withPWAConfig({
+  dest: 'public',
+  // Solution: https://github.com/shadowwalker/next-pwa/issues/424#issuecomment-1399683017
+  buildExcludes: ['app-build-manifest.json'],
+  register: false,
+  skipWaiting: false,
+  clientsClaim: true,
+  swSrc: './src/services/worker/service-worker.js',
+  offlineGoogleAnalytics: true
+});
+
+module.exports = withSentryConfig(
+  withPWA(nextConfig),
   {
     // For all available options, see:
     // https://github.com/getsentry/sentry-webpack-plugin#options
@@ -60,5 +97,3 @@ const nextConfigWithSentry = withSentryConfig(
     disableLogger: true
   }
 );
-
-module.exports = nextConfigWithSentry;
