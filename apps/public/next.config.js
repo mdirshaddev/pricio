f8148ac15b6311bb753b5aff69bd3fbdce07c336
mdirshaddev <mdirshaddev+github@gmail.com>
2023-09-05 22:45:05 +0530

    config: next config with pwa and sentry integration plugin
    * note some third party image hosts are being whitelisted for using with next images
    * next-pwa plugin uses workbox-webpack-plugin with custom worker using inject manifest for service worker file generation
    * @sentry/nextjs plugin uses with integration of next-pwa plugin

--- apps/public/next.config.js
+++ apps/public/next.config.js
@@ -3,6 +3,12 @@ const withPWAConfig = require('next-pwa');
 const { version } = require('./package.json');
 const { withSentryConfig } = require('@sentry/nextjs');
 
+/**
+ * The function `generateAppDirEntry` registers a service worker on the app directory in a Next.js
+ * project.
+ * @returns The function `generateAppDirEntry` returns a promise that resolves to an object containing
+ * the entries.
+ */
 const generateAppDirEntry = entry => {
   const packagePath = require.resolve('next-pwa');
   const packageDirectory = path.dirname(packagePath);
@@ -21,6 +27,8 @@ const generateAppDirEntry = entry => {
   });
 };
 
+/* The code block you provided is defining the `nextConfig` object, which is a configuration object for
+Next.js. It specifies various settings and options for the Next.js application. */
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -51,17 +59,25 @@ const nextConfig = {
   }
 };
 
+/* The code block you provided is creating a configuration object for Next.js with the help of the
+`withPWAConfig` function from the `next-pwa` package. This configuration object is used to enable
+Progressive Web App (PWA) features in the Next.js application. */
 /** @type {import('next-pwa').PWAConfig} */
 const withPWA = withPWAConfig({
   dest: 'public',
+  disable: process.env.NODE_ENV !== 'production',
   // Solution: https://github.com/shadowwalker/next-pwa/issues/424#issuecomment-1399683017
   buildExcludes: ['app-build-manifest.json'],
   register: false,
   skipWaiting: false,
   clientsClaim: true,
+  // Use inject manifest as we are using custom service worker
   swSrc: './src/services/worker/service-worker.js'
 });
 
+/* The code block `module.exports = withSentryConfig(...)` is exporting a modified version of the
+`nextConfig` object with additional configurations for Sentry, a popular error tracking and
+monitoring tool. */
 module.exports = withSentryConfig(
   withPWA(nextConfig),
   {
